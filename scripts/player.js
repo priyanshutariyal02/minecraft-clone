af832800731c4fea6b434197e6f452302629b4ef
Priyanshu Tariyal <138609182+priyanshutariyal02@users.noreply.github.com>
2025-07-30 00:10:19 +0530

    feat: implement collision detection system with broad and narrow phases

--- scripts/player.js
+++ scripts/player.js
@@ -2,11 +2,16 @@ import * as THREE from "three";
 import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
 
 export class Player {
+  radius = 0.5;
+  height = 1.75;
+  jumpSpeed = 10;
+  onGround = false;
   maxSpeed = 10;
   input = new THREE.Vector3();
   velocity = new THREE.Vector3();
+  #worldVelocity = new THREE.Vector3();
   camera = new THREE.PerspectiveCamera(
-    70,
+    91,
     window.innerWidth / window.innerHeight,
     0.1,
     200
@@ -21,8 +26,33 @@ export class Player {
     this.position.set(32, 16, 32);
     scene.add(this.camera);
     scene.add(this.cameraHelper);
+
     document.addEventListener("keydown", this.onKeyDown.bind(this));
     document.addEventListener("keyup", this.onKeyUp.bind(this));
+
+    // wireframe mesh visualizing the player bounding cyliner
+    this.boundHelper = new THREE.Mesh(
+      new THREE.CylinderGeometry(this.radius, this.radius, this.height, 16),
+      new THREE.MeshBasicMaterial({ wireframe: true })
+    );
+    scene.add(this.boundHelper);
+  }
+
+  get worldVelocity() {
+    this.#worldVelocity.copy(this.velocity);
+    this.#worldVelocity.applyEuler(
+      new THREE.Euler(0, this.camera.rotation.y, 0)
+    );
+    return this.#worldVelocity;
+  }
+
+  /**
+   * apply the change in velocity "dv" that is specified in the world frame
+   * @param {THREE.Vector3} dv
+   */
+  applyWorldDaltaVelocity(dv) {
+    dv.applyEuler(new THREE.Euler(0, this.camera.rotation.y, 0));
+    this.velocity.add(dv);
   }
 
   applyInput(dt) {
@@ -31,9 +61,18 @@ export class Player {
       this.velocity.z = this.input.z;
       this.controls.moveRight(this.velocity.x * dt);
       this.controls.moveForward(this.velocity.z * dt);
+      this.position.y += this.velocity.y * dt;
 
       document.getElementById("player-position").innerHTML = this.toString();
     }
+  }
+
+  /**
+   * updates the position of the player's bounding helper cylinder
+   */
+  updateBoundHelper() {
+    this.boundHelper.position.copy(this.position);
+    this.boundHelper.position.y -= this.height / 2;
   }
 
   /**
@@ -68,8 +107,14 @@ export class Player {
         this.input.x = this.maxSpeed;
         break;
       case "KeyR":
-        this.position.set(32, 16, 32);
+        // this.position.set(32, 16, 32);
+        this.position.y = 32;
         this.velocity.set(0, 0, 0);
+        break;
+      case "Space":
+        if (this.onGround) {
+          this.velocity.y += this.jumpSpeed;
+        }
         break;
     }
   }
