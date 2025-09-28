b630c6a648b8306fdf22af7bbc9603a3e88b0fa6
Priyanshu Tariyal <138609182+priyanshutariyal02@users.noreply.github.com>
2025-09-28 18:13:33 +0530

    feat: adding logic of add & and remove chunk dynamically

--- scripts/world.js
+++ scripts/world.js
@@ -2,8 +2,18 @@ import * as THREE from "three";
 import { WorldChunk } from "./worldChunk";
 
 export class World extends THREE.Group {
+  /**
+   * The number of chunks to render around the player.
+   * When this set to 0, the chunk the player is on
+   * is the only one that is rendered. If it is set of 1,
+   * the adjecent chunk are rendered; if set to 2, the
+   * chunks adjecet to those are rendered, and so on.
+   */
+
+  drawDistance = 2;
+
   chunkSize = {
-    width: 64,
+    width: 32,
     height: 32,
   };
 
@@ -11,7 +21,7 @@ export class World extends THREE.Group {
     seed: 0,
     terrain: {
       scale: 30,
-      magnitude: 0.5,
+      magnitude: 0.05,
       offset: 0.2,
     },
   }; // pass these parameters into each chunks when we are initiation it
@@ -27,8 +37,8 @@ export class World extends THREE.Group {
 
   generate() {
     this.disposeChunks();
-    for (let x = -1; x <= 1; x++) {
-      for (let z = -1; z <= 1; z++) {
+    for (let x = -this.drawDistance; x <= this.drawDistance; x++) {
+      for (let z = -this.drawDistance; z <= this.drawDistance; z++) {
         const chunk = new WorldChunk(this.chunkSize, this.params);
         chunk.position.set(
           x * this.chunkSize.width,
@@ -40,6 +50,117 @@ export class World extends THREE.Group {
         this.add(chunk);
       }
     }
+  }
+
+  /**
+   * Update the visible position of the world based on the current player position
+   * @param {Player} player
+   */
+  update(player) {
+    // Steps:
+    // 1. Find visible chunkes based on player's current position
+    const visibleChunks = this.getVisinleChunks(player);
+    // 2. Compare with the current set of chunks
+    const chunksToAdd = this.getChunksToAdd(visibleChunks);
+    // 3. Remove chunks that are not longer visible
+    this.removeUnusedChunks(visibleChunks);
+    // 4. Add new chunks that just came into view
+    for (const chunk of chunksToAdd) {
+      this.generateChunk(chunk.x, chunk.z);
+    }
+  }
+
+  /**
+   * Return an array containing the coordinates of the chunks that are currently visiblw to the player
+   * @param {Player} player
+   * @returns {{x: number, z: number}[]}
+   */
+  getVisinleChunks(player) {
+    const visibleChunks = [];
+
+    const coords = this.worldToChunkCoords(
+      player.position.x,
+      player.position.y,
+      player.position.z
+    );
+
+    const chunkX = coords.chunk.x;
+    const chunkZ = coords.chunk.z;
+
+    for (
+      let x = chunkX - this.drawDistance;
+      x <= chunkX + this.drawDistance;
+      x++
+    ) {
+      for (
+        let z = chunkZ - this.drawDistance;
+        z <= chunkZ + this.drawDistance;
+        z++
+      ) {
+        visibleChunks.push({ x, z });
+      }
+    }
+
+    return visibleChunks;
+  }
+
+  /**
+   * Returns an array containing the coordinates of the chunks that
+   * are not yet loaded and need to be added to the scene
+   * @param {{ x: number, z: number}[]} visibleChunks
+   * @returns {{ x: number, z: number}[]}
+   */
+  getChunksToAdd(visibleChunks) {
+    // Filter down the visible chunks to those not already in the world
+    return visibleChunks.filter((chunk) => {
+      const chunkExists = this.children
+        .map((obj) => obj.userData)
+        .find(({ x, z }) => chunk.x === x && chunk.z === z);
+
+      return !chunkExists;
+    });
+  }
+
+  /**
+   * Removes current loaded chunks that are no longer visible to the player
+   * @param {{ x: number, z: number}[]} visibleChunks
+   */
+  removeUnusedChunks(visibleChunks) {
+    // Filter down the visible chunks to those not already in the world
+    const chunksToRemove = this.children.filter((chunk) => {
+      const { x, z } = chunk.userData;
+      const chunkExists = visibleChunks.find(
+        (visibleChunk) => visibleChunk.x === x && visibleChunk.z === z
+      );
+
+      return !chunkExists;
+    });
+
+    for (const chunk of chunksToRemove) {
+      chunk.disposeInstances();
+      this.remove(chunk);
+      console.log(
+        `Removing chunk at X: ${chunk.userData.x}, Z: ${chunk.userData.z}`
+      );
+    }
+  }
+
+  /**
+   * Generate the chunk at (x, z) coordinates
+   * @param {number} x
+   * @param {number} z
+   */
+  generateChunk(x, z) {
+    const chunk = new WorldChunk(this.chunkSize, this.params);
+    chunk.position.set(
+      x * this.chunkSize.width * 1.01,
+      0,
+      z * this.chunkSize.width * 1.01
+    );
+    chunk.userData = { x, z }; // store x & z coordinates
+    chunk.generate();
+    this.add(chunk);
+    console.log(`Adding chunk at X: ${x}, Z: ${z}`);
   }
 
   /**
